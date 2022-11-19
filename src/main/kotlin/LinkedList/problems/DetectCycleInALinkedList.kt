421ad074b511aa4f246179080035352534ce32a3
Raghav Aggarwal <raghavaggarwal776@gmail.com>
2022-11-19 16:45:49 +0530

    Detect Cycle in a LinkedList

---
+++ src/main/kotlin/LinkedList/problems/DetectCycleInALinkedList.kt
@@ -0,0 +1,10 @@
+package LinkedList.problems
+
+
+fun main() {
+
+    // submission link
+    // trick - use fast and slow pointer technique
+    // https://leetcode.com/problems/linked-list-cycle/submissions/846262982/
+
+}
